56c231477d2ac57b6865c1106280f4fccf824e08
Hankerr <15109212549@163.com>
2019-05-21 23:59:27 +0800

    增加登录功能

--- j2ee/src/main/java/HelloServlet.java
+++ j2ee/src/main/java/HelloServlet.java
@@ -5,9 +5,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class HelloServlet extends HttpServlet{
+public class HelloServlet extends HttpServlet {
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response){
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
 
         try {
             response.getWriter().println("<h1>Hello Servlet!</h1>");
--- j2ee/src/main/java/LoginServlet.java
+++ j2ee/src/main/java/LoginServlet.java
@@ -4,29 +4,31 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Date;
 
 //@WebServlet(name = "Login")
 public class LoginServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         try {
-        String name=request.getParameter("name");
-        String password=request.getParameter("password");
+            this.doPost(request, response);
+            String name = request.getParameter("name");
+            String password = request.getParameter("password");
 
-        System.out.printf("name："+name);
-        System.out.printf("password:"+password);
+            String html=null;
+            if("admin".equals(name)&&"123".equals(password)){
+
+            }else{
+
+            }
+            System.out.printf("name：" + name);
+            System.out.printf("password:" + password);
         } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    }
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        try {
-            response.getWriter().println("<h1>Hello Servlet!</h1>");
-            response.getWriter().println(new Date().toString());
-        } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
