2d758b3e9d236eef6682af15e31f1fae909267cd
Hankerr <15109212549@163.com>
2019-05-21 22:25:01 +0800

    增加登录功能

--- j2ee/src/main/java/LoginServlet.java
+++ j2ee/src/main/java/LoginServlet.java
@@ -4,16 +4,31 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 
-@WebServlet(name = "Login")
+//@WebServlet(name = "Login")
 public class LoginServlet extends HttpServlet {
-    public void doPost(HttpServletRequest request,
-                          HttpServletResponse response)
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        try {
         String name=request.getParameter("name");
         String password=request.getParameter("password");
 
         System.out.printf("name："+name);
         System.out.printf("password:"+password);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            response.getWriter().println("<h1>Hello Servlet!</h1>");
+            response.getWriter().println(new Date().toString());
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
